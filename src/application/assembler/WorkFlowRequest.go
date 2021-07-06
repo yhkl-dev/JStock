3c944bd461b1761d8092d6f910def7f57490d0fa
yhkl-dev <kaiyang939325@gmail.com>
2021-07-06 22:33:29 +0800

    update

--- src/application/assembler/WorkFlowRequest.go
+++ src/application/assembler/WorkFlowRequest.go
@@ -2,4 +2,4 @@ package assembler
 
 type WorkFlowAddRequest struct{}
 
-func (s *WorkFlowAddRequest) D2M_WorkFlow(dto *dto.WorkFlowAddRequest) {}
+// func (s *WorkFlowAddRequest) D2M_WorkFlow(dto *dto.WorkFlowAddRequest) {}
--- src/application/services/WorkflowMainService.go
+++ src/application/services/WorkflowMainService.go
@@ -2,18 +2,22 @@ package services
 
 import (
 	"JStock/src/application/assembler"
+	"JStock/src/application/dto"
+	frontworkflowtempate "JStock/src/domain/aggs/frontWorkflowTempate"
+	workflowtemplate "JStock/src/domain/models/workFlowTemplate"
+	"JStock/src/infrastructure/GormDao"
 
-	"gorm.io/gorm")
-
+	"gorm.io/gorm"
+)
 
 type WorkFlowMainService struct {
 	AssWorkFlowAddReq *assembler.WorkFlowAddRequest
-	AssWorkFlowAddReq *assembler.FlowTemplateAddRequest
-	DB *gorm.DB `inject:"-"`
+	AssWorkFlowAddRsp *assembler.WorkFlowAddRequest
+	DB                *gorm.DB `inject:"-"`
 }
 
 func (s *WorkFlowMainService) CreateWorkFlowTemplate(req *dto.FlowTemplateAddRequest) *dto.FlowTemplateAddResponse {
-	flowTemplateModel := s.AssFlowTemplateAddReq.D2M_FlowTemplateMain(req)
+	flowTemplateModel := s.AssWorkFlowAddReq.D2M_FlowTemplateMain(req)
 	ItemModel := workflowtemplate.NewWorkFlowItemTempate()
 	repo1 := GormDao.NewIFlowTemplateRepo(s.DB)
 	repo2 := GormDao.NewItemMainRepo(s.DB)
